64ecc9680da34c9bd8b701b20ba9cac92c6d290f
Rom <rom@Book.local>
2017-05-12 22:02:13 +0300

    add validators and dafault values for strings, TODO = pass only strings

--- server/server.js
+++ server/server.js
@@ -5,17 +5,23 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }, 
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({ });
+// by default mongoose try convert things to string == can pass text: true / 100 + 1 (101)... 
+let newTodo = new Todo({ text: 1 + 2 });
 
 newTodo.save().then((res) => {
     console.log(res);
